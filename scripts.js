351ef6e15af72c4e5bb2d5acd0569f8106de4c36
jhonoi <comillennium@gmail.com>
2022-10-30 18:07:24 -0500

    website complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', function(){
+    let submitButton = document.getElementsByTagName('button')[0];
+    let messageBox = document.getElementsByClassName('message')[0];
+    let emailBox = document.getElementById('email');
+
+    submitButton.addEventListener('click', function(e){
+        if(emailBox.value.includes('@') && emailBox.value.includes('.')){
+            messageBox.innerHTML = 'Thank you! Your email address ' + emailBox.value + ' has been added to our mailing list';
+        }else{
+            messageBox.innerHTML = 'Please enter a valid email address.';
+        }
+        e.preventDefault();
+    })
+})
